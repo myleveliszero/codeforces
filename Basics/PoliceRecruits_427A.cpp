b8d445bea0d87cc5b7f0aa35e633bbde81ce2cbf
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-02-27 19:42:21 +0500

    difficulty "A"

---
+++ Basics/PoliceRecruits_427A.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+// 97-122 // a-z
+// 65-90 // A-Z
+
+#define pb push_back
+#define YES cout << "YES" << "\n"
+#define NO cout << "NO" << "\n"
+#define pout(x) cout << x << "\n"
+#define vout(x) for(auto c: x){ cout << c << ' ';} {cout << "\n";}
+#define vmax(x)  *max_element(x.begin(), x.end())
+#define vmin(x)  *min_element(x.begin(), x.end())
+#define lower(x) for(auto& c : x){ c = tolower(c); }
+#define upper(x) for(auto& c : x){ c = toupper(c); }
+
+
+void viRead(vector<int>& vi){
+    int n, dummy;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> dummy;
+        vi.pb(dummy);
+    }
+}
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    vector<int> events;
+    viRead(events);
+    int capacity = 0;
+    int cnt = 0;
+    for(int i=0; i < events.size(); i++){
+        if (events[i] == -1){
+            if (capacity > 0){
+                capacity -= 1;
+            }
+            else{
+                cnt++;
+            }
+        }
+        else{
+            capacity += events[i];
+        }
+    }
+    pout(cnt);
+
+    return 0;
+}
