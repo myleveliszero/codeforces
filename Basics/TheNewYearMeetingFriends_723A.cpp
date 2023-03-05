c42964cb3fded87bd55329f2537e56239a2366d7
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-03-05 09:13:44 +0500

    difficulty "A"

---
+++ Basics/TheNewYearMeetingFriends_723A.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+// 97-122 // a-z
+// 65-90 // A-Z
+
+#define pb push_back
+#define YES cout << "YES" << "\n"
+#define NO cout << "NO" << "\n"
+#define pout(x) cout << x << "\n"
+#define vout(x) for(auto c: x){ cout << c << ' ';} {cout << "\n";}
+#define vmax(x)  *max_element(x.begin(), x.end())
+#define vmin(x)  *min_element(x.begin(), x.end())
+#define lower(x) for(auto& c : x){ c = tolower(c); }
+#define upper(x) for(auto& c : x){ c = toupper(c); }
+
+
+void viRead(vector<int>& vi){
+    int n, dummy;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> dummy;
+        vi.pb(dummy);
+    }
+}
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int x1,x2,x3;
+    cin >> x1 >> x2 >> x3;
+    // three cases
+    // x1
+    int d1 = abs(x1-x2)+abs(x1-x3);
+    int d2 = abs(x2-x1)+abs(x2-x3);
+    int d3 = abs(x3-x1)+abs(x3-x2);
+    pout(min({d1,d2,d3}));
+
+    return 0;
+}
