3186ae13e21467ba008271965cd7fe3ada5962d3
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-03-05 09:33:54 +0500

    difficulty "A"

---
+++ Basics/GameWithSticks_451A.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+// 97-122 // a-z
+// 65-90 // A-Z
+
+#define pb push_back
+#define YES cout << "YES" << "\n"
+#define NO cout << "NO" << "\n"
+#define pout(x) cout << x << "\n"
+#define vout(x) for(auto c: x){ cout << c << ' ';} {cout << "\n";}
+#define vmax(x)  *max_element(x.begin(), x.end())
+#define vmin(x)  *min_element(x.begin(), x.end())
+#define lower(x) for(auto& c : x){ c = tolower(c); }
+#define upper(x) for(auto& c : x){ c = toupper(c); }
+
+
+void viRead(vector<int>& vi){
+    int n, dummy;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> dummy;
+        vi.pb(dummy);
+    }
+}
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, m;
+    cin >> n >> m;
+    if (n == m or n < m){
+        if (n % 2 == 0){
+            pout("Malvika");
+        }
+        else{
+            pout("Akshat");
+        }
+    }
+    else{
+        if (m%2 == 0){
+            pout("Malvika");
+        }
+        else{
+            pout("Akshat");
+        }
+    }
+
+    return 0;
+}
