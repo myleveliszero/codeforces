fdc5cc754299b9a3812aad849f738d4be8da5889
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-03-08 18:35:16 +0500

    difficulty "A"

---
+++ Basics/YetAnotherTwoIntegersProblem_1409A.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+// 97-122 // a-z
+// 65-90 // A-Z
+
+#define pb push_back
+#define YES cout << "YES" << "\n"
+#define NO cout << "NO" << "\n"
+#define pout(x) cout << x << "\n"
+#define vout(x) for(auto c: x){ cout << c << ' ';} {cout << "\n";}
+#define vmax(x)  *max_element(x.begin(), x.end())
+#define vmin(x)  *min_element(x.begin(), x.end())
+#define lower(x) for(auto& c : x){ c = tolower(c); }
+#define upper(x) for(auto& c : x){ c = toupper(c); }
+
+
+void viRead(vector<int>& vi){
+    int n, dummy;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> dummy;
+        vi.pb(dummy);
+    }
+}
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t; 
+    cin >> t;
+    int a,b;
+    while (t--){
+        cin >> a >> b;
+        if (a == b){
+            pout(0);
+        }
+        else if (a < b){
+            int cnt = b-a;
+            if (cnt % 10 == 0 ){
+                pout(cnt/10);
+            }
+            else{
+                pout(cnt/10+1);
+            }
+        }
+        else{
+            int cnt = a-b;
+            if (cnt % 10 == 0 ){
+                pout(cnt/10);
+            }
+            else{
+                pout(cnt/10+1);
+            }
+        }
+    }
+
+    return 0;
+}
