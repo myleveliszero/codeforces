a0b869ba3ca225593f7fc1b5a9777d48ac535232
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-03-19 09:47:13 +0500

    difficulty "A"

---
+++ Basics/MishkaAndGame_703A.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+// 97-122 // a-z
+// 65-90 // A-Z
+
+#define pb push_back
+#define YES cout << "YES" << "\n"
+#define NO cout << "NO" << "\n"
+#define pout(x) cout << x << "\n"
+#define vout(x) for(auto c: x){ cout << c << ' ';} {cout << "\n";}
+#define vmax(x)  *max_element(x.begin(), x.end())
+#define vmin(x)  *min_element(x.begin(), x.end())
+#define lower(x) for(auto& c : x){ c = tolower(c); }
+#define upper(x) for(auto& c : x){ c = toupper(c); }
+
+
+void viRead(vector<int>& vi){
+    int n, dummy;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> dummy;
+        vi.pb(dummy);
+    }
+}
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    int m,c;
+    int l = 0, r = 0;
+    for(int i=0; i<n; i++){
+        cin >> m >> c;
+        if (m < c){
+            r++;
+        }
+        else if (m > c){
+            l++;
+        }
+        else{
+            r++; l++;
+        }
+    }
+    if (l < r){
+        pout("Chris");
+    }
+    else if (l > r){
+        pout("Mishka"); 
+    }
+    else{
+        pout("Friendship is magic!^^");
+    }
+
+    return 0;
+}
