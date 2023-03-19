ff88c1d5bff092b02a6eca6232dd8669e79d637d
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-03-19 09:13:52 +0500

    difficulty "A"

---
+++ Basics/AgainTwentyFive_630A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+// 97-122 // a-z
+// 65-90 // A-Z
+
+#define pb push_back
+#define YES cout << "YES" << "\n"
+#define NO cout << "NO" << "\n"
+#define pout(x) cout << x << "\n"
+#define vout(x) for(auto c: x){ cout << c << ' ';} {cout << "\n";}
+#define vmax(x)  *max_element(x.begin(), x.end())
+#define vmin(x)  *min_element(x.begin(), x.end())
+#define lower(x) for(auto& c : x){ c = tolower(c); }
+#define upper(x) for(auto& c : x){ c = toupper(c); }
+
+
+void viRead(vector<int>& vi){
+    int n, dummy;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> dummy;
+        vi.pb(dummy);
+    }
+}
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    //find last two digits of number 5^n
+    // 5*5 = 25 25*5=125 125*5=625 625*5=3125 3125*5=15625
+    cout << 25; 
+
+    return 0;
+}
