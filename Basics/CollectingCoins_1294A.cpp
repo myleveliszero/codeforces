adbf82b5bd00af388352d3fbf904b6eaedba2055
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-04-04 09:32:08 +0500

    difficulty "A"

---
+++ Basics/CollectingCoins_1294A.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+// 97-122 // a-z
+// 65-90 // A-Z
+
+#define pb push_back
+#define YES cout << "YES" << "\n"
+#define NO cout << "NO" << "\n"
+#define pout(x) cout << x << "\n"
+#define vout(x) for(auto c: x){ cout << c << ' ';} {cout << "\n";}
+#define vmax(x)  *max_element(x.begin(), x.end())
+#define vmin(x)  *min_element(x.begin(), x.end())
+#define vsort(x) sort(x.begin(), x.end())
+#define vsum(x, valtype) accumulate(x.begin(), x.end(), valtype)
+#define lower(x) for(auto& c : x){ c = tolower(c); }
+#define upper(x) for(auto& c : x){ c = toupper(c); }
+
+
+void viRead(vector<int>& vi){
+    int n, dummy;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> dummy;
+        vi.pb(dummy);
+    }
+}
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        int a,b,c,n;
+        cin >> a >> b >> c >> n;
+        int mn,mx;
+        mx = max({a,b,c});
+        int diff = 0;
+        if (mx == a){
+            diff += a-b;
+            diff += a-c;
+        }
+        else if (mx == b){
+            diff += b-a;
+            diff += b-c;
+
+        }
+        else if(mx == c){
+            diff += c-a;
+            diff += c-b;
+        }
+
+        if (diff > n){
+            NO;
+        }
+        else{
+            int res = n-diff;
+            if (res%3 == 0){
+                YES;
+            }
+            else{
+                NO;
+            }
+        }
+
+    }
+    
+    return 0;
+}
