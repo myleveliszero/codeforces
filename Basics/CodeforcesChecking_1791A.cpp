ef81d2f788e0c910380c6bbae15eb0909031f9c8
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-04-05 18:14:41 +0500

    difficulty "A"

---
+++ Basics/CodeforcesChecking_1791A.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+// 97-122 // a-z
+// 65-90 // A-Z
+
+#define pb push_back
+#define YES cout << "YES" << "\n"
+#define NO cout << "NO" << "\n"
+#define pout(x) cout << x << "\n"
+#define vout(x) for(auto c: x){ cout << c << ' ';} {cout << "\n";}
+#define vmax(x)  *max_element(x.begin(), x.end())
+#define vmin(x)  *min_element(x.begin(), x.end())
+#define vsort(x) sort(x.begin(), x.end())
+#define vsum(x, valtype) accumulate(x.begin(), x.end(), valtype)
+#define lower(x) for(auto& c : x){ c = tolower(c); }
+#define upper(x) for(auto& c : x){ c = toupper(c); }
+
+
+void viRead(vector<int>& vi){
+    int n, dummy;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> dummy;
+        vi.pb(dummy);
+    }
+}
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //string codeforce = "codeforces";
+    int t; cin >> t;
+    while(t--){
+        char c; cin >> c;
+        if (c == 'c'){
+            YES;
+        }
+        else if (c == 'o'){
+            YES;
+        }
+        else if (c == 'd'){
+            YES;
+        }
+        else if (c == 'e'){
+            YES;
+        }
+        else if (c == 'f'){
+            YES;
+        }
+        else if (c == 'r'){
+            YES;
+        }
+        else if (c == 's'){
+            YES;
+        }
+        else{
+            NO;
+        }
+    } 
+
+
+    return 0;
+}
