d5e9b2c6260fbae885f997449e4665170ac04b74
myleveliszero <120106705+myleveliszero@users.noreply.github.com>
2023-04-19 20:21:16 +0500

    difficulty "A"

---
+++ Basics/MaximumInTable_509A.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+// 97-122 // a-z
+// 65-90 // A-Z
+
+#define pb push_back
+#define YES cout << "YES" << "\n"
+#define NO cout << "NO" << "\n"
+#define pout(x) cout << x << "\n"
+#define vout(x) for(auto c: x){ cout << c << ' ';} {cout << "\n";}
+#define vmax(x)  *max_element(x.begin(), x.end())
+#define vmin(x)  *min_element(x.begin(), x.end())
+#define vsort(x) sort(x.begin(), x.end())
+#define vsum(x, valtype) accumulate(x.begin(), x.end(), valtype)
+#define lower(x) for(auto& c : x){ c = tolower(c); }
+#define upper(x) for(auto& c : x){ c = toupper(c); }
+
+
+void viRead(vector<int>& vi){
+    int n, dummy;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> dummy;
+        vi.pb(dummy);
+    }
+}
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    vector<int> v;
+    for(int i=0; i<n; i++){
+        v.pb(1);
+    }
+    
+    for(size_t i=0; i < n-1; i++){
+        vector<int> res;
+        res.pb(1);
+        for(size_t j=0; j < n-1; j++){
+            res.pb(v[j+1] + res[j]);
+        }
+        v = res;
+    }
+    pout(v[v.size()-1]);
+    
+    return 0;
+}
